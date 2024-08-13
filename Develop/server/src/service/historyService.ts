9cc3527bd1b890831ae78eaaab81a1ce26c8075c
Kaleb Boothe <kboothe617@gmail.com>
2024-08-13 00:23:49 -0600

    addcity method historyservice.ts

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -37,11 +37,16 @@ class HistoryService {
   }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   async getCities(): Promise<City[]> {
-    const cities = await this.read();
-    return cities;
+    return this.read();
   }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(name: string): Promise<void> {
+    const cities = await this.read();
+    const id = Date.now().toString(); // Generating a simple ID using timestamp
+    const newCity = new City(id, name);
+    cities.push(newCity);
+    await this.write(cities);
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
