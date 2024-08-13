1504a3cc6f7f71217c3ce59a90c223c2f6409952
Kaleb Boothe <kboothe617@gmail.com>
2024-08-13 00:21:11 -0600

    allied code with comments better

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -26,7 +26,7 @@ class HistoryService {
       return [];
     }
   }
-  // private async read() {}
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   private async write(cities: City[]): Promise<void> {
     try {
       const data = JSON.stringify(cities, null, 2);
@@ -35,13 +35,11 @@ class HistoryService {
       console.error('Error writing to file:', error);
     }
   }
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  async getCities(): Promise<City[]> {
-    return this.read();
-  }
-  // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities(): Promise<City[]> {
+    const cities = await this.read();
+    return cities;
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
